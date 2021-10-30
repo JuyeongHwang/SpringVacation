5da9949df3dc1354086595478033600f2f4a2437
이다흥 <coca0105@naver.com>
2021-10-30 10:36:45 +0900

    도구 이미지 추가 외
    상점에 이용될 도구 이미지를 추가하였습니다
    
    결과화면에서도 도구 변경이 가능하도록 하였습니다
    
    간단하게만 상점 UI를 건들어보았습니다
    
    도현체를 추가하였습니다

---
+++ Spring/Assets/Spring/Scripts/resultScript/KidController_Result.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KidController_Result : MonoBehaviour
+{
+    [Header ("애니메이터 설정")]
+    // 시작하기 전에 사용자로부터 지정받아야 하도록 설정
+    public Animator kidAnimator;
+    public Animator[] kidToolAnimators; // 지정 받아야 하는 애니메이터
+    protected Animator kidToolAnimator; // 인덱스를 이용한 현재 애니메이터
+    public int kidToolIndex;
+
+    void Start ()
+    {
+        SetChildToolByIndex (kidToolIndex);
+        SetAnimatorTrigger ("Idle");
+    }
+
+    public void SetAnimatorTrigger (string triggerName)
+    {
+        // 몸체 애니메이터
+        if (kidAnimator == null)
+            return;
+
+        kidAnimator.SetTrigger (triggerName);
+
+        // 툴 애니메이터
+        if (kidToolAnimator == null)
+            return;
+        
+        kidToolAnimator.SetTrigger (triggerName);
+    }
+
+    public void SetChildToolByIndex (int index)
+    {
+        if (0 <= kidToolIndex && kidToolIndex < kidToolAnimators.Length
+        && kidToolAnimators [kidToolIndex])
+        {
+            kidToolAnimator = kidToolAnimators [kidToolIndex];
+
+            for (int i = 0; i < kidToolAnimators.Length; i++)
+            {
+                if (i == index)
+                {
+                    kidToolAnimators [i].gameObject.SetActive (true);
+                }
+                else
+                {
+                    kidToolAnimators [i].gameObject.SetActive (false);
+                }
+            }
+        }
+    }
+}

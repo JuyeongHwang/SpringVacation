c4da92c31467516f49bb0e4a2331690f152268c4
황주영 <49023736+JuyeongHwang@users.noreply.github.com>
2021-12-12 18:05:38 +0900

    BEACH
    sea 오브젝트 fav, 야자수 => BEACH 로 설정

--- Spring/Assets/Spring/Scripts/terrainGenerator/EnvManager.cs
+++ Spring/Assets/Spring/Scripts/terrainGenerator/EnvManager.cs
@@ -886,13 +886,6 @@ public class EnvManager : MonoBehaviour
                 continue;
             }
 
-            //해변일 경우 pass
-
-            if(e.name == "Palm_Tree")
-            {
-                continue;
-            }
-
             float dist = Vector3.Distance (e.gameObject.transform.position, originPos);
 
             // 후보 등록
@@ -995,7 +988,7 @@ public class EnvManager : MonoBehaviour
             // 일반 땅 : Random.Range (0, bugPrefabs.Length-3), 
             // 해변가 : Random.Range (4, bugPrefabs.Length)
 
-            int index = Random.Range (0, bugPrefabs.Length-3);
+            int index = Random.Range (0, bugPrefabs.Length);
             float rotY = Random.Range (0, 360f);
 
             ret = Instantiate (bugPrefabs [index], pos, Quaternion.Euler (Vector3.up * rotY), bugHolder.transform);
